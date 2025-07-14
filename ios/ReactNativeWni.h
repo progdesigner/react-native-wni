0e0e4b1529dc5d409761de8af6a53ddae8278fe5
John <john@progdesigner.com>
2025-07-14 15:02:00 +0900

    fix(ios): 헤더 파일에 webView, isEmulator 프로퍼티 추가 및 빌드 오류 수정

--- ios/ReactNativeWni.h
+++ ios/ReactNativeWni.h
@@ -3,5 +3,6 @@
 #import <React/RCTBridgeModule.h>
 
 @interface ReactNativeWni : NSObject <RCTBridgeModule>
-
+@property (nonatomic, strong) WKWebView *webView;
+@property (nonatomic, assign) BOOL isEmulator;
 @end
